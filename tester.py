78507192c886f7c2a0e9c53283710b6463b4cff4
Kelvin Li <ltwisabc@gmail.com>
2012-08-01 16:17:50 -0700

    tester.py: convert "print" to Python 3 syntax

--- tester.py
+++ tester.py
@@ -5,7 +5,7 @@ import synbioinformatica, sys
 from synbioinformatica import DNA, restrictionEnzyme, Ligate
 
 def main(template, primer_1, primer_2):
-	print PCR(template, primer_1, primer_2)
+	print(PCR(template, primer_1, primer_2))
 
 if __name__ == "__main__":
 	InitializeEnzymes = getattr(synbioinformatica,'EnzymeDictionary')
@@ -15,46 +15,46 @@ if __name__ == "__main__":
 	DigestBuffer = getattr(synbioinformatica,'DigestBuffer')
 	GelAndZymoPurify = getattr(synbioinformatica,'GelAndZymoPurify')
 	EnzymeDictionary = InitializeEnzymes()
-	print 'buffers:'
-	# print DigestBuffer("BglII", "BamHI")
+	print('buffers:')
+	# print(DigestBuffer("BglII", "BamHI"))
 	plasmid = DNA('ATGGTGAGATGAGTGAAGGCGAGCTGGTGGATGCATTCCGCCATGTGAGTGATGCGTTTGAGCAAACCAGCGAAACCATCGGCGTGCGCGCCAATAACGCGATCAACGACATGGTGCGTCAACGTCTGCTGAACCGCTTTACCAGCGAGCAGGCGGAAGGGAACGCAATTTACCGTCTGACGCCGCTCGGCATCGGCATTACTGACTACNNNATCCGTCAGCGCGAGTTTTCTACGCTGCGTCTTTCTATGCAGTTGTCGATTGTGGCGGGTGAGCTCAAACGCGCAGCAGATGCCGCCGAAGAGGGCGGTGATGAATTTCACTGGCACCGTAATGTCTATGCGCCACTGAAATATTCGGTAGCAGAAATTTTCGACAGTATCGACCTGACGCAACGTCTGATGGACGAACAGCAGCAGCAGGTGAAGGACGATATCGCCCAGTTGCTGAACAAAGACTGGCGGGCGGCGATTTCCAGCTGTGAATTGTTGCTTTCGGAAACTTCCGGAACGCTGCGTGAATTGCAGGATACGCTGGAAGCGGCAGGCGACAAATTGCAGGCTAATCTGTTGCGCATTCAGGATGCGACGATGACCCATGACGATCTGCATTTCGTCGATCGTCTGGTGTTCGATCTGCAGAGCAAACTCGATCGTATTATCAGTTGGGGCCAGCAATCCATCGACTTGTGGATTGGCTACGACCGCCACGTACACAAATTTATTCGTACCGCGATCGATATGGATAAAAACCGCGTCTTTGCTCAGCGGTTACGTCAGTCGGTACAAACCTATTTTGATGAGCCGTGGGCGCTAACTTATGCCAATGCCGATCGTCTGCTGGATATGCGTGACGAAGAGATGGCACTGCGCGATGAAGAAGTGACTGGGGAACTTCCTGAGGATCTGGAATACGAAGAGTTTAACGAGATCCGCGAACAGCTGGCGGCGATCATCGAAGAACAACTTGCCGTGTACAAAACCAGACAAGTGCCGCTGGATCTTGGTCTGGTGGTACGCGAATATCTGTCACAGTATCCGCGTGCACGTCACTTTGACGTTGCGCGTATTGTTATTGATACCTGACGCAACGTCTGC','plasmid','bNE015')
 	primer_1 = DNA("NNNNNNNNNCTTCTTCATCGCGCAGTGCCATCT",'primer','ne001')
 	primer_2 = DNA("NNNNNNNNNGTGAAGGCGAGCTGGTGGATGC",'primer','ne002')
 	pcrProduct = PCR(primer_1, primer_2, plasmid)
 	pcrProduct.prettyPrint()
-	print pcrProduct.sequence
-	print "\n\tPCR product instructions: "+pcrProduct.instructions+"\n"
-	print "\t"+str(pcrProduct.parents)+"\n"
-	print "\t"+str(pcrProduct.children)+"\n"
+	print(pcrProduct.sequence)
+	print("\n\tPCR product instructions: "+pcrProduct.instructions+"\n")
+	print("\t"+str(pcrProduct.parents)+"\n")
+	print("\t"+str(pcrProduct.children)+"\n")
 	# # products = Digest(plasmid,[EnzymeDictionary["BsaAI"]])
 	# # products = Digest(plasmid,[EnzymeDictionary["EcoRI"], EnzymeDictionary["BceAI"]])
 	plasmid = DNA('aatgctactactattagtagaattgatgccaccttttcagctcgcgccccaaatgaaaatatagctaaacaggttattgaccatttgcgaaatgtatctaatggtcaaactaaatctactcgttcgcagaattgggaatcaactgttatatggaatgaaacttccagacaccgtactttagttgcatatttaaaacatgttgagctacagcattatattcagcaattaagctctaagccatccgcaaaaatgacctcttatcaaaaggagcaattaaaggtactctctaatcctgacctgttggagtttgcttccggtctggttcgctttgaagctcgaattaaaacgcgatatttgaagtctttcgggcttcctcttaatctttttgatgcaatccgctttgcttctgactataatagtcagggtaaagacctgatttttgatttatggtcattctcgttttctgaactgtttGGATCCaaagcatttgagggggattcaatgaatatttatgacgattccgcagtattggacgctatccagtctaaacattttactattaccccctctggcaaaacttcttttgcaaaagcctctcgctattttggtttttatcgtcgtctggtaaacgagggttatgatagtgttgctcttactatgcctcgtaattccttttggcgttatgtatctgcattagttgaatgtggtattcctaaatctcaactgatgaatctttctacctgtaataatgttgttccgttagttcgttttattaacgtagatttttcttcccaacgtcctgactggtataatgagccagttcttaaaatcgcataaggtaattcacaatgattaaagttgaaattaaaccatctcaagcccaatttactactcgttctggtgtttctcgtcagggcaagccttattcactgaatgagcagctttgttacgttgatttgggtaatgaatatccggttcttgtcaagattactcttgatgaaggtcagccagcctatgcgcctggtctgtacaccgttcatctgtcctctttcaaagttggtcagttcggttcccttatgattgaccgtctgcgccAGATCTcttcagaccAGATCTagacaccgtactttagttgcatatttaaaacatgttgagctacagcattatattcagcaattaagctctaagccatccgcaaaaatgacctcttatcaaaaggagcaattaaaggtactctctaatcctgacctgttggagtttgcttccggtctggttcgctttgaagctcgaattaaaacgcgatatttgaagtctttcgggcttcctcttaatctttttgatgcaatccgctttgcttctgactataatagtcagggtaaagacctgatttttgatttatggtcattctcgttttctgaactgtttGGATCCaaagcatttgagggggattcaatgaatatttatgacgattccgcagtattggacgctatccagtctaaacattttactattaccccctctggcaaaacttcttttgcaaaagcctctcgctattttggtttttatcgtcgtctggtaaacgagggttatgatagtgttgctcttactatgcctcgtaattccttttggcgttatgtatctgcattagttgaatgtggtattcctaaatctcaactgatgaatctttctacctgtaataatgttgttccgttagttcgttttattaacgtagatttttcttcccaacgtcctgactggtataatgagccagttcttaaaatcgcataaggtaattcacaatgattaaagttgaaattaaaccatctcaagcccaatttactactcgttctggtgtttctcgtcagggcaagccttattcactgaatgagcagctttgttacgttgatttgggtaatgaatatccggttcttgtcaagattactcttgatgaaggtcagccagcctatgcgcctggtctgtacaccgttcatctgtcctctttcaaagttggtcagttcggttcccttatgattgaccgtctgcgccAGATCTaatgctactactattagtagaattgatgccaccttttcagctcgcgccccaaatgaaaatatagctaaacaggttattgaccatttgcgaaatgtatctaatggtcaaactaaatctactcgttcgcagaattgggaatcaactgttatatggaatgaaacttccagacaccgtactttagttgcatatttaaaacatgttgagctacagcattatattcagcaattaagctctaagccatccg','plasmid','bNE100')
 	products = Digest(plasmid,[EnzymeDictionary["BamHI"], EnzymeDictionary["BglII"]])
 	for product in products:
-		print "\tDigest Fragment instructions: "+product.instructions+"\n\n"
+		print("\tDigest Fragment instructions: "+product.instructions+"\n\n")
 	bands = GelAndZymoPurify(products,'L')
 	for band in bands:
-		print "\tGelAndZymo Fragment instructions: "+band.instructions+"\n\n"
+		print("\tGelAndZymo Fragment instructions: "+band.instructions+"\n\n")
 	ligated = Ligate(bands)
 	for lig in ligated:
-		print "\tLigation product instructions: "+lig.instructions+"\n\n"
-	print '\nInput DNA:\n'
+		print("\tLigation product instructions: "+lig.instructions+"\n\n")
+	print('\nInput DNA:\n')
 	plasmid.prettyPrint()
-	print '\nDigestion product(s):\n'
+	print('\nDigestion product(s):\n')
 	n = 1
 	for product in products:
-		print 'digest #'+str(n)+':'
+		print('digest #'+str(n)+':')
 		product.prettyPrint()
 		n = n + 1
-	print '\nGel Purification product(s):\n'
+	print('\nGel Purification product(s):\n')
 	n = 1
 	for product in bands:
-		print 'purified #'+str(n)+':'
+		print('purified #'+str(n)+':')
 		product.prettyPrint()
 		n = n + 1
-	print "\nLigation product(s):\n"
+	print("\nLigation product(s):\n")
 	n = 1
 	for product in ligated:
-		print 'ligation product #'+str(n)+':'
+		print('ligation product #'+str(n)+':')
 		product.prettyPrint()
 		n = n + 1
